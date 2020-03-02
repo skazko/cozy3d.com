e9fa3700c8106c25b99f922dd89af9beddd97451
Slava <kreeon@ya.ru>
2020-03-02 23:12:33 +0300

    rework mobile menu and header

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -5,21 +5,18 @@
  * navigation support for dropdown menus.
  */
 ( function() {
-	var gamburger, gamburgerButton, container, button, menu, links, i, len;
+	let container, button, menu, links, i, len;
 
 	container = document.getElementById( 'site-navigation' );
 	if ( ! container ) {
 		return;
 	}
 
-	gamburger = document.getElementById( 'gamburger' );
-
-	gamburgerButton = document.querySelector('.gamburger-inner');
-
 	button = container.getElementsByTagName( 'button' )[0];
-	// if ( 'undefined' === typeof button ) {
-	// 	return;
-	// }
+	if ( 'undefined' === typeof button ) {
+		console.log('button not finded')
+		return;
+	}
 
 	menu = container.getElementsByTagName( 'ul' )[0];
 
@@ -30,40 +27,69 @@
 	}
 
 	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
+	if ( -1 === menu.className.indexOf( 'main-navigation__menu' ) ) {
+		menu.classList.add('main-navigation__menu');
 	}
 
-	// button.onclick = function() {
-	// 	if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-	// 		container.className = container.className.replace( ' toggled', '' );
-	// 		button.setAttribute( 'aria-expanded', 'false' );
-	// 		menu.setAttribute( 'aria-expanded', 'false' );
-	// 	} else {
-	// 		container.className += ' toggled';
-	// 		button.setAttribute( 'aria-expanded', 'true' );
-	// 		menu.setAttribute( 'aria-expanded', 'true' );
-	// 	}
-	// };
+	document.addEventListener('closeMobileMenu', () => {
+		console.log('close mobile menu');
+	})
 
-	gamburgerButton.onclick = function() {
+	document.addEventListener('openMobileMenu', () => {
+		console.log('open mobile menu');
+	})
+
+	const mobile = window.matchMedia('(min-width: 640px)');
+	function closeMenuHandler(mql) {
+		if (document.body.dataset.mobileMenuIsOpened && mql.matches) {
+			console.log('close menu handler')
+			delete document.body.dataset.mobileMenuIsOpened;
+			container.classList.remove('toggled');
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+			menu.classList.remove('main-navigation__menu_open');
+		}
+	}
+
+	mobile.addListener(closeMenuHandler)
+
+	button.onclick = function() {
 		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-					container.className = container.className.replace( ' toggled', '' );
-					menu.setAttribute( 'aria-expanded', 'false' );
-				} else {
-					container.className += ' toggled';
-					menu.setAttribute( 'aria-expanded', 'true' );
-				}
+			document.dispatchEvent(new CustomEvent('closeMobileMenu'));
+			delete document.body.dataset.mobileMenuIsOpened;
+			container.classList.remove('toggled');
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+			menu.classList.add('main-navigation__menu_animate');
+			menu.classList.remove('main-navigation__menu_open');
+			animation = setTimeout(() => {
+				menu.classList.remove('main-navigation__menu_animate');
+			},300)
+		} else {
+			document.dispatchEvent(new CustomEvent('openMobileMenu'));
+			window.scrollTo(0, 0);
+			document.body.dataset.mobileMenuIsOpened = true;
+			container.classList.add('toggled');
+			menu.classList.add('main-navigation__menu_animate');
+			menu.classList.add('main-navigation__menu_open');
+			animation = setTimeout(() => {
+				menu.classList.remove('main-navigation__menu_animate');
+			},300)
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
 	};
 
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
 
-	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].addEventListener( 'focus', toggleFocus, true );
-		links[i].addEventListener( 'blur', toggleFocus, true );
-	}
+
+	// // Get all the link elements within the menu.
+	// links    = menu.getElementsByTagName( 'a' );
+
+	// // Each time a menu link is focused or blurred, toggle focus.
+	// for ( i = 0, len = links.length; i < len; i++ ) {
+	// 	links[i].addEventListener( 'focus', toggleFocus, true );
+	// 	links[i].addEventListener( 'blur', toggleFocus, true );
+	// }
 
 	/**
 	 * Sets or removes .focus class on an element.
@@ -90,31 +116,31 @@
 	/**
 	 * Toggles `focus` class to allow submenu access on tablets.
 	 */
-	( function( container ) {
-		var touchStartFn, i,
-			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+		// ( function( container ) {
+		// 	var touchStartFn, i,
+		// 		parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
-		if ( 'ontouchstart' in window ) {
-			touchStartFn = function( e ) {
-				var menuItem = this.parentNode, i;
+		// 	if ( 'ontouchstart' in window ) {
+		// 		touchStartFn = function( e ) {
+		// 			var menuItem = this.parentNode, i;
 
-				if ( ! menuItem.classList.contains( 'focus' ) ) {
-					e.preventDefault();
-					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
-						if ( menuItem === menuItem.parentNode.children[i] ) {
-							continue;
-						}
-						menuItem.parentNode.children[i].classList.remove( 'focus' );
-					}
-					menuItem.classList.add( 'focus' );
-				} else {
-					menuItem.classList.remove( 'focus' );
-				}
-			};
+		// 			if ( ! menuItem.classList.contains( 'focus' ) ) {
+		// 				e.preventDefault();
+		// 				for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+		// 					if ( menuItem === menuItem.parentNode.children[i] ) {
+		// 						continue;
+		// 					}
+		// 					menuItem.parentNode.children[i].classList.remove( 'focus' );
+		// 				}
+		// 				menuItem.classList.add( 'focus' );
+		// 			} else {
+		// 				menuItem.classList.remove( 'focus' );
+		// 			}
+		// 		};
 
-			for ( i = 0; i < parentLink.length; ++i ) {
-				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
-			}
-		}
-	}( container ) );
+		// 		for ( i = 0; i < parentLink.length; ++i ) {
+		// 			parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
+		// 		}
+		// 	}
+		// }( container ) );
 } )();
--- src/templates/header.php
+++ src/templates/header.php
@@ -17,60 +17,65 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'kateslava' ); ?></a>
-
+	<a class="skip-link screen-reader-text" href="#main">
+		<?php esc_html_e( 'Skip to content', 'kateslava' ); ?>
+	</a>
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			if ( has_custom_logo()) :
+		<?php	if ( has_custom_logo()) : ?>
+			<h1 class="screen-reader-text"><?php bloginfo( 'name' ); ?></h1>				
+			<?php the_custom_logo();
+		else: 
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</h1>
+			<?php else : ?>
+				<p class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</p>
+			<?php	endif;
+			$kateslava_description = get_bloginfo( 'description', 'display' );
+			if ( $kateslava_description || is_customize_preview() ) :
 				?>
-					<h1 class="screen-reader-text"><?php bloginfo( 'name' ); ?></h1>				
-				<?php
-				the_custom_logo();
-			else: 
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				$kateslava_description = get_bloginfo( 'description', 'display' );
-				if ( $kateslava_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $kateslava_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif;
-			endif; ?>
-		</div><!-- .site-branding -->
-
-
-		<?php if (esc_html( get_option('phone') )) {
-				?>
-					<div class="wa-button-mob">
-						<a href="https://wa.me/<?php echo preg_replace('/[^0-9]/', '', esc_html( get_option( 'phone', '' ) ));?>"><img width="32px" height="32px" src="<?php echo get_template_directory_uri();?>/img/icons/wa.svg" alt="whatsapp"></a>
-					</div>
-				<?php
-			} ?>
+				<p class="site-description"><?php echo $kateslava_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif;
+		endif; ?>
 		
-
-		<input id="gamburger" type="checkbox">
-		<label class="gamburger-inner" for="gamburger">
-			<div></div>
-			<div></div>
-			<div></div>
-		</label>
-
 		<nav id="site-navigation" class="main-navigation">
-			
-			<?php
-			wp_nav_menu( array(
+			<button type="button" class="main-navigation__toggler">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
+					fill="none" 
+					stroke="currentColor" 
+					stroke-width="2" 
+					stroke-linecap="round" 
+					stroke-linejoin="round"
+					class="icon-open" >
+					<line x1="3" y1="12" x2="21" y2="12"></line>
+					<line x1="3" y1="6" x2="21" y2="6"></line>
+					<line x1="3" y1="18" x2="21" y2="18"></line>
+				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" 
+					fill="none" 
+					stroke="currentColor" 
+					stroke-width="2" 
+					stroke-linecap="round" 
+					stroke-linejoin="round"
+					class="icon-close" >
+					<line x1="18" y1="6" x2="6" y2="18"></line>
+					<line x1="6" y1="6" x2="18" y2="18"></line>
+				</svg>
+			</button>
+			<?php wp_nav_menu( array(
 				'theme_location' => 'menu-1',
+				'container'      => false,
 				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+				'menu_class'     => 'main-navigation__menu',
+			) ); ?>
+		</nav>
 
 		<div class="social-links">
 			<?php if (esc_html( get_option('phone') )) {
