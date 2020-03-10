940c224164bd7ef7c6548ad8a814d7e7977f4bb5
Slava <kreeon@ya.ru>
2020-03-10 18:56:24 +0300

    add portfolio styles to archive pages

--- src/templates/archive.php
+++ src/templates/archive.php
@@ -14,29 +14,25 @@ get_header();
 		<main id="main" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
+			<section class="portfolio">
+				<header class="portfolio__header">
+					<?php
+					the_archive_title( '<h1 class="portfolio__title">', '</h1>' ); ?>
+					<p class="portfolio__description"> 
+						<?php echo	strip_tags(get_the_archive_description()); ?>
+					</p>
+				</header>
+				<ul class="portfolio__list">
+					<?php
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/content', 'portfolio' );
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+					endwhile;?>
+				</ul>
+				<?php the_posts_navigation(); ?>
+			</section>
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'archive' );
-
-			endwhile;
-
-			the_posts_navigation();
 
 		else :
 
