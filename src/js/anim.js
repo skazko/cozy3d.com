74a4812972e48f9bb5bd9ec81e495e390a4be2e6
Slava <kreeon@ya.ru>
2020-03-20 16:18:05 +0300

    add anim function

--- src/js/anim.js
+++ src/js/anim.js
@@ -1,116 +1,29 @@
+import {anim} from './src/anim/index';
+
 (function() {
-  const desktopMediaQuery = '(min-width: 1024px)';
-  const portfolio = document.querySelector('.portfolio');
-  if (!portfolio) {
-    return;
-  }
-
-  const desktop = window.matchMedia(desktopMediaQuery);
-
-  const items = portfolio.querySelectorAll('.portfolio__item');
-  const list = portfolio.querySelector('.portfolio__list');
-
-  const intersictionOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.01
-  };
-
-  const parallax = {
-    desktop: function(elem, title) {
-      const gap = (window.pageYOffset - elem.offsetTop) * 0.25;
-      title.style.transform = 'translateY(' + gap + 'px)';
-    },
-    mobile: function(elem, title) {
-      // const gap = (window.pageYOffset - elem.offsetTop) * 0.25;
-      if (!elem.dataset.opacity) {
-        elem.dataset.opacity = 0;
-      }
-      if (elem.dataset.opacity < 100) {
-        elem.dataset.opacity = +elem.dataset.opacity + 10;
-      }
-    }
-  }
-
-  desktop.addListener(function(mql) {
-    const items = portfolio.querySelectorAll('.portfolio__item');
-    const handler = mql.matches ? parallax.desktop : parallax.mobile;
-
-    if (!mql.matches) {
-      items.forEach(item => {
-        const title = item.querySelector('.project__title');
-        title.style.transform = '';
+  function mobileHandler(item) {
+    const title = item.querySelector('.project__title');
+    return function() {
+      window.requestAnimationFrame(function() {
+        item.dataset.mobile = window.pageYOffset;
       })
     }
-
-    listeners.forEach(listener => {
-      window.removeEventListener('scroll', listener);
-    })
-
-    items.forEach(item => {
-      const title = item.querySelector('.project__title');
-      listeners[item.dataset.position] = function() {
-        window.requestAnimationFrame(function() {
-          handler(item, title);
-        });
-      };
-    });
-  });
-
-
-  function init(elem, listeners) {
-    elem.dataset.position = listeners.length;
-    const title = elem.querySelector('.project__title');
-    const handler = desktop.matches ? parallax.desktop : parallax.mobile;
-
-    listeners.push(function() {
-      window.requestAnimationFrame(function () {
-        handler(elem, title);        
-      });
-    });
-  } 
-
-  const listeners = [];
-
-  const callback = function(entries, observer) {
-    console.log('callback')
-    entries.forEach(entry => {
-
-      if (!entry.target.dataset.initialized) {
-        init(entry.target, listeners);
-        if (entry.isIntersecting) {
-          listeners[entry.target.dataset.position]();
-        }
-      } 
-      
-      if (entry.isIntersecting) {
-        window.addEventListener('scroll', listeners[entry.target.dataset.position]);
-      } else {
-        window.removeEventListener('scroll', listeners[entry.target.dataset.position]);
-      }
-    })
-  };
-
-  const observer = new IntersectionObserver(callback, intersictionOptions);
-
-  for (let i = 0; i < items.length; i++) {
-    observer.observe(items.item(i));
   }
 
-  //mutation observer
+  function desktopHandler(item) {
+    const title = item.querySelector('.project__title');
+    return function() {
+      window.requestAnimationFrame(function() {
+        item.dataset.desktop = window.pageYOffset;
+      })
+    }
+  }
 
-  let mutationObserver = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => {
-        if (Node.ELEMENT_NODE === node.nodeType && node.classList.contains('portfolio__item')) {
-          init(node, listeners);
-          observer.observe(node);
-        }
-      });
-    });
-  });
-
-  mutationObserver.observe(list, {
-    childList: true,
+  anim({
+    breakpoint: '(min-width: 1024px)',
+    itemsClassName: 'portfolio__item',
+    listClassName: 'portfolio__list',
+    mobileHandler,
+    desktopHandler
   });
 })();
---
+++ src/js/src/anim/index.js
@@ -0,0 +1,90 @@
+function clearSideEffects(elem, initial) {
+  elem.outerHTML = initial[elem.dataset.position];
+}
+
+function clearScrollHandlers(handlers) {
+  handlers.forEach(handler => window.removeEventListener('scroll', handler));
+  handlers.length = 0;
+}
+
+function initItem(item, initial, handlers, createHandler) {
+  initial.push(item.outerHTML);
+  item.dataset.position = handlers.length;
+  handlers.push(createHandler(item));
+}
+
+function startIntersectionObserver({ items, createHandler, handlers, initial }) {
+  const defaultSettings = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.01
+  };
+
+  function callback(entries, observer) {
+    entries.forEach(entry => {
+      if (!entry.target.dataset.position) {
+        initItem(entry.target, initial, handlers, createHandler);
+        console.log(entry)
+        if (entry.isIntersecting) {
+          handlers[entry.target.dataset.position]();
+        }
+      } 
+      
+      if (entry.isIntersecting) {
+        window.addEventListener('scroll', handlers[entry.target.dataset.position]);
+      } else {
+        window.removeEventListener('scroll', handlers[entry.target.dataset.position]);
+      }
+    })
+  }
+  const observer = new IntersectionObserver(callback, defaultSettings);
+
+  for (let i = 0; i < items.length; i++) {
+    observer.observe(items.item(i));
+  }
+
+  return observer;
+}
+
+export function anim(settings) {
+  const {breakpoint, itemsClassName, listClassName, desktopHandler, mobileHandler} = settings;
+  const handlers = [];
+  const initial = [];
+  const items = document.getElementsByClassName(itemsClassName);
+  if ( items.length === 0 ) {
+    return;
+  }
+  const breakpointMQ = window.matchMedia(breakpoint);
+
+  let createHandler = breakpointMQ.matches ? desktopHandler : mobileHandler;
+
+  let intersectionObserver = startIntersectionObserver({ createHandler, items, handlers, initial });
+
+  breakpointMQ.addListener(function(mql) {
+    for (let i = 0; i < items.length; i++) {
+      clearSideEffects(items[i], initial)
+    }
+    clearScrollHandlers(handlers);
+    initial.length = 0;
+    createHandler = mql.matches ? desktopHandler : mobileHandler;
+    intersectionObserver = startIntersectionObserver({ createHandler, items, handlers, initial})
+  });
+
+  const list = document.querySelector('.' + listClassName);
+  if (list) {
+    let mutationObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          if (Node.ELEMENT_NODE === node.nodeType && node.classList.contains(itemsClassName)) {
+            initItem(node, initial, handlers, createHandler);
+            intersectionObserver.observe(node);
+          }
+        });
+      });
+    });
+  
+    mutationObserver.observe(list, {
+      childList: true,
+    });
+  }
+}
